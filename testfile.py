072c109e416d99b163f8bbdef737cada291653d0
Dan Ford <dpford@gmail.com>
2013-05-04 14:16:05 -0400

    dans stuff

--- testfile.py
+++ testfile.py
@@ -1,4 +1,5 @@
 from trollop import TrelloConnection
+import datetime
 
 conn = TrelloConnection('8e08cc15f33eba483bc2ec18f5d9d2e8', '546c06039131159676b3f7c41c295e6cb0529417861c27f0287122ceadcf3065')
 
@@ -37,18 +38,40 @@ member_cards = hackathon.members[1].cards
 print "*** Get member counts ***"
 board_members = hackathon.members
 
-print "Create a list for each member"
+print "Create a dict entry for each member"
 mem_count = {}
 for member in board_members:
-	print "Setup: " + str(member.fullname)
 	mem_count[member.fullname] = 0
 
 print mem_count
 
+print "Populate dict"
 for card in hackathon.cards:
 	for member in card.members:
-		print member.fullname
-		print card
 		mem_count[member.fullname] += 1
 
 print mem_count
+
+print "Count of each list"
+list_count = {}
+for list in hackathon.lists:
+    list_count[list.name] = len(list.cards)
+    
+print list_count
+
+todaydate = datetime.datetime.now()
+late_master = []
+for card in hackathon.cards:
+    if card._data['due']:
+        duedate = datetime.datetime.strptime(card._data['due'][:19], '%Y-%m-%dT%H:%M:%S')
+        if duedate < todaydate:
+            late_dict = {}
+            late_members = []
+            for member in card.members:
+                late_members.append(member.fullname)
+            late_dict['members'] = late_members
+            late_dict['title'] = card.name
+            late_dict['description'] = card.desc
+            late_dict['due'] = card._data['due'][:10]
+            print late_dict
+            late_master.append(late_dict)
