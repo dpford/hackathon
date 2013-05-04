14567727f606b4a79492382308c41044770f70fe
Andrew T. Baker <andrew.tork.baker@gmail.com>
2013-05-04 14:50:34 -0400

    Template updates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.cache import never_cache
 
 from trollop import TrelloConnection
 
+@never_cache
 def home(request):
 	data = {}
 	conn = TrelloConnection('8e08cc15f33eba483bc2ec18f5d9d2e8', '546c06039131159676b3f7c41c295e6cb0529417861c27f0287122ceadcf3065')
