739d7cefb7e50a1af93872e06289c187ba97cd8a
Dan Ford <dpford@gmail.com>
2013-05-06 17:10:37 -0400

    new fixes

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,48 +8,49 @@ import datetime
 
 @never_cache
 def home(request):
-	data = {}
-	conn = TrelloConnection('8e08cc15f33eba483bc2ec18f5d9d2e8', '546c06039131159676b3f7c41c295e6cb0529417861c27f0287122ceadcf3065')
-	hackathon = conn.me.boards[4]
-	board_members = hackathon.members
+    data = {}
+    conn = TrelloConnection('8e08cc15f33eba483bc2ec18f5d9d2e8', '546c06039131159676b3f7c41c295e6cb0529417861c27f0287122ceadcf3065')
+    hackathon = conn.me.boards[4]
+    board_members = hackathon.members
 
-	# Create a dict entry for each member
-	mem_count = {}
-	for member in board_members:
-		mem_count[member.fullname] = 0
+    # Create a dict entry for each member
+    mem_count = {}
+    for member in board_members:
+        mem_count[member.fullname] = 0
 
-	# Populate dict
-	for card in hackathon.cards:
-		for member in card.members:
-			mem_count[member.fullname] += 1
+    # Populate dict
+    for card in hackathon.cards:
+        for member in card.members:
+            mem_count[member.fullname] += 1
 
-	data['assignments'] = mem_count
+    data['assignments'] = mem_count
 
-	# Current status - count of each list
-	list_count = {}
-	for list in hackathon.lists:
-		list_count[list.name] = len(list.cards)
+    # Current status - count of each list
+    list_count = {}
+    for list in hackathon.lists:
+        list_count[list.name] = len(list.cards)
 
-	data['list_count'] = list_count
+    data['list_count'] = list_count
 
-	# Overdue stories
-	todaydate = datetime.datetime.now()
-	late_master = []
-	for card in hackathon.cards:
-		if card._data['due']:
-			duedate = datetime.datetime.strptime(card._data['due'][:19], '%Y-%m-%dT%H:%M:%S')
-			if duedate < todaydate:
-				late_dict = {}
-				late_members = []
-				for member in card.members:
-					late_members.append(member.fullname)
-				late_dict['members'] = late_members
-				late_dict['title'] = card.name
-				late_dict['description'] = card.desc
-				late_dict['due'] = card._data['due'][:10]
-				print late_dict
-				late_master.append(late_dict)
+    # Overdue stories
+    todaydatetime = datetime.datetime.now()
+    todaydate = datetime.datetime.date(todaydatetime)
+    late_master = []
+    for card in hackathon.cards:
+        if card._data['due'] and card.list.name != 'Done':
+            duedatetime = datetime.datetime.strptime(card._data['due'][:19], '%Y-%m-%dT%H:%M:%S')
+            duedate = datetime.datetime.date(duedatetime)
+            if duedate <= todaydate:
+                late_dict = {}
+                late_members = []
+                for member in card.members:
+                    late_members.append(member.fullname)
+                late_dict['members'] = late_members
+                late_dict['title'] = card.name
+                late_dict['description'] = card.desc
+                late_dict['due'] = card._data['due'][:10]
+                late_master.append(late_dict)
 
-	data['late_stories'] = late_master	
+    data['late_stories'] = late_master    
 
-	return render(request, 'index.html', data)
+    return render(request, 'index.html', data)
