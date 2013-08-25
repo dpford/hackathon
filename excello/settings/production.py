3b57c2ba9a8cda842a491d4b46fd994f476d1e53
Dan <dpford@gmail.com>
2013-08-25 13:44:36 -0400

    switched bucket name

--- excello/settings/production.py
+++ excello/settings/production.py
@@ -3,8 +3,8 @@ from base import *
 # Production storage using s3.
 DEFAULT_FILE_STORAGE = 's3storages.MediaStorage'
 STATICFILES_STORAGE = 's3storages.StaticStorage'
-STATIC_URL = 'http://excello.s3-website-us-east-1.amazonaws.com/static/'
-ADMIN_MEDIA_PREFIX = 'http://excello.s3-website-us-east-1.amazonaws.com/static/admin/'
-MEDIA_URL = 'http://excello.s3-website-us-east-1.amazonaws.com/media/'
+STATIC_URL = 'http://excello2.s3-website-us-east-1.amazonaws.com/static/'
+ADMIN_MEDIA_PREFIX = 'http://excello2.s3-website-us-east-1.amazonaws.com/static/admin/'
+MEDIA_URL = 'http://excello2.s3-website-us-east-1.amazonaws.com/media/'
 
-AWS_S3_CUSTOM_DOMAIN = 'http://excello.s3-website-us-east-1.amazonaws.com/static' #important: no "http://"
+AWS_S3_CUSTOM_DOMAIN = 'http://excello2.s3-website-us-east-1.amazonaws.com/static' #important: no "http://"
--- s3storages.py
+++ s3storages.py
@@ -2,10 +2,10 @@ from storages.backends.s3boto import S3BotoStorage
 
 # Define bucket and folder for static files.
 StaticStorage = lambda: S3BotoStorage(
-    bucket='excello', 
+    bucket='excello2', 
     location='static')
 
 # Define bucket and folder for media files.
 MediaStorage  = lambda: S3BotoStorage(
-    bucket='excello',
+    bucket='excello2',
     location='media')
