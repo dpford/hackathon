42399a30cf1d9607fa524a81bd09f702750668ad
Dan <dpford@gmail.com>
2013-08-25 13:52:18 -0400

    s3 dir fix

--- excello/settings/production.py
+++ excello/settings/production.py
@@ -3,8 +3,8 @@ from base import *
 # Production storage using s3.
 DEFAULT_FILE_STORAGE = 's3storages.MediaStorage'
 STATICFILES_STORAGE = 's3storages.StaticStorage'
-STATIC_URL = 'http://excello2.s3-website-us-east-1.amazonaws.com/static/'
-ADMIN_MEDIA_PREFIX = 'http://excello2.s3-website-us-east-1.amazonaws.com/static/admin/'
-MEDIA_URL = 'http://excello2.s3-website-us-east-1.amazonaws.com/media/'
+STATIC_URL = 'https://s3.amazonaws.com/excello2/static/'
+ADMIN_MEDIA_PREFIX = 'https://s3.amazonaws.com/excello2/static/admin/'
+MEDIA_URL = 'https://s3.amazonaws.com/excello2/media/'
 
-AWS_S3_CUSTOM_DOMAIN = 'http://excello2.s3-website-us-east-1.amazonaws.com/static' #important: no "http://"
+AWS_S3_CUSTOM_DOMAIN = 'https://s3.amazonaws.com/excello2/static' #important: no "http://"
