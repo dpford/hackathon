c92f84da08e83be5eca51447ee4c4c062c8da462
Dan <dpford@gmail.com>
2013-09-23 13:52:19 -0400

    checking in old changes

--- excello/urls.py
+++ excello/urls.py
@@ -19,3 +19,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatters += patters('',
+    (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views', {'document_root': settings.STATIC_ROOT}),
+    )
