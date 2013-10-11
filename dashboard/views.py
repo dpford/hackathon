7a2486aae12366101661c775f457be3aab2258b9
Dan <dpford@gmail.com>
2013-10-10 21:18:39 -0400

    runtime. redirects

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
